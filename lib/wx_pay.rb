8d564b53495158c9acf88fd6f4ff7ec7485852c6
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-04 22:54:09 +0800

    move methods to utils

--- lib/wx_pay.rb
+++ lib/wx_pay.rb
@@ -5,28 +5,11 @@ require 'wx_pay/version'
 require 'openssl'
 require 'zeitwerk'
 require 'active_support/configurable'
-#loader = Zeitwerk::Loader.for_gem
-#loader.setup # ready!
+loader = Zeitwerk::Loader.for_gem
+loader.setup # ready!
 
 module WxPay
   include ActiveSupport::Configurable
-  extend self
-
-  def set_apiclient_by_pkcs12(str, pass)
-    pkcs12 = OpenSSL::PKCS12.new(str, pass)
-    @apiclient_cert = pkcs12.certificate
-    @apiclient_key = pkcs12.key
-
-    pkcs12
-  end
-
-  def apiclient_cert=(cert)
-    @apiclient_cert = OpenSSL::X509::Certificate.new(cert)
-  end
-
-  def apiclient_key=(key)
-    @apiclient_key = OpenSSL::PKey::RSA.new(key)
-  end
 
   configure do |config|
     config.sandbox = false
--- lib/wx_pay/utils.rb
+++ lib/wx_pay/utils.rb
@@ -23,5 +23,21 @@ module WxPay
       end
     end
 
+    def set_apiclient_by_pkcs12(str, pass)
+      pkcs12 = OpenSSL::PKCS12.new(str, pass)
+      @apiclient_cert = pkcs12.certificate
+      @apiclient_key = pkcs12.key
+
+      pkcs12
+    end
+
+    def apiclient_cert=(cert)
+      @apiclient_cert = OpenSSL::X509::Certificate.new(cert)
+    end
+
+    def apiclient_key=(key)
+      @apiclient_key = OpenSSL::PKey::RSA.new(key)
+    end
+
   end
 end
